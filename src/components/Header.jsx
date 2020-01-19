f92b4b86434d2482b72974486125ceba58f5cc85
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-01-18 21:42:10 -0300

    Explicando que es .jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+// JSX = Version Mejorada de JS , si quieres mostrar dentro del return algo de JS
+// se debe colocar entre {}
+
 function Header() {
+
+    // JSX
+    /*
+    const edad = 18;
+
+    let mensaje;
+
+    if (edad >= 18) {
+        mensaje = "Eres Mayor de Edad"
+    }else{
+        mensaje = "Eres Menor de Edad"
+    }*/
+
+    // Este return es lo que se ve en pantalla 
     return(
-        <h1>Desde el Header</h1>
+
+        <h1 id ="encabezado" className ="encabezado">Tienda Virtual</h1>
+        // <h1 id ="encabezado" className ="encabezado">{mensaje}</h1>
     )
 }
 
