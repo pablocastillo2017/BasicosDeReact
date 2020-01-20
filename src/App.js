de581acec22a1906a47f1679f2f1bb035fd2658a
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-01-20 14:08:09 -0400

    41.- como mostrar una coleccion del state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React,{Fragment} from 'react';
+import React,{Fragment , useState } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Producto from './components/Producto'
 
 
 
 function App() {
 
-  // Obtener Fecha
 
-  const fecha = new Date().getFullYear();
+  // Crear listado de productos
+  // useState extrae dos valores el 1- State(productos) 
+  // funcion para ree-escribir el State o setear (guardarProductos)
+  
+  const [ productos, guardarProductos ] = useState([
+
+    { id: 1, nombre: 'Camisa ReactJS', precio: 50 },
+    { id: 2, nombre: 'Camisa VueJS', precio: 40 },
+    { id: 3, nombre: 'Camisa Node.js', precio: 30 },
+    { id: 4, nombre: 'Camisa Angular', precio: 20 },
+  ]);
+
+
+  // Obtener Fecha
+  const fecha = new Date().getUTCFullYear();
+
+  
+
 
   return (
     
@@ -17,6 +34,20 @@ function App() {
         <Header
           titulo='Tienda Virtual'
         />
+        
+        <h1>Lista de Productos</h1>
+        
+        {productos.map(producto =>(
+
+          <Producto
+
+            key={producto.id}
+            producto={producto}
+          />
+
+        ))}
+
+
 
         <Footer
           fecha={fecha}
