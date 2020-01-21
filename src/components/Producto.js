2078829fb6deda59a2348a9600547c35efbdc4fd
Pablo Castillo <pablocastilloastudillo@gmail.com>
2020-01-21 00:53:33 -0400

    4.42 Eventos React

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -4,11 +4,22 @@ const Producto = ({producto}) => {
 
     const{nombre,precio,id} = producto
 
+    // Funcion para Agregar Producto Al carrito
+
+    const seleccionarProducto = id =>{
+        console.log("Comprando...", id);
+    }
+
     return ( 
         <div>
 
             <h2>{nombre}</h2>
             <p>${precio}</p>
+            <button type="button" onClick={()=> seleccionarProducto(id)}>
+                
+                Comprar
+                
+                </button> 
             
 
         </div>
